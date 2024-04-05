65521f2cf9d159cc6495bc403332a42aedeb3650
Nathan Walsh <nwalsh@stratence.partners>
2024-04-05 11:35:11 +0200

    clean up tests: csv (string,number,bool)

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -7,8 +7,6 @@ import numpy as np
 import os
 import glob
 
-import eel.config as ec
-
 from eel.cli import execute
 from eel.execute import pandas_end_points
 from eel.path import get_config_default
@@ -35,13 +33,13 @@ handler.setFormatter(formatter)
 # Add the handler to the logger
 logger.addHandler(handler)
 
-logger.info("Getting Started")
-
 
 @pytest.fixture(autouse=True, scope="session")
 def setup():
     os.chdir(os.path.join(".", "temp"))
-    # start_logging()
+    logger.info("Getting Started")
+
+    # remove files in the temp directory
     temp_files = glob.glob("*.*")
     for file in temp_files:
         if not file.endswith(".log"):
@@ -53,36 +51,11 @@ def test_cwd():
     assert "d:\\Sync\\repos\\eel\\temp" == os.getcwd()
 
 
-# TODO: move this to eel.config?
-def build_default_config() -> ec.Config:
-    return ec.Config()
-
-
-def is_numeric(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
 # Get the ec.Config dictionary for a given DataFrame
 def get_df_config(df: pd.DataFrame) -> dict:
     config_df = get_config_default()
-    # if config_df.source.read_csv is None:
-    #     config_df.source.read_csv = ec.ReadCsv()
-    # config_df.source.read_csv.dtype = df.dtypes.apply(lambda x: x.name).to_dict()
     config_df.source.dtype = df.dtypes.apply(lambda x: x.name).to_dict()
-
-    # this is to prevent empty strings from being read as NaN:
-    # config_df.source.read_csv.keep_default_na = False
-    # config_df.source.read_csv.na_values = ["nan"]
-    # = ["NaN", "Null", "null", "#NA", "N/A", "#N/A"]
-
-    # config_df.source.read_csv.na_values = None
-
     config_dict = config_df.model_dump(exclude_none=True)
-
     return config_dict
 
 
@@ -90,24 +63,6 @@ def test_enum_conversion():
     config = get_config_default()
     assert config.target.consistency == "strict"
     assert config.target.if_exists == "fail"
-
-
-# # Test python type to numpy type conversion and euqality
-# @pytest.mark.parametrize(
-#     "py_val, dtypes",
-#     [
-#         (1, (int, np.int32)),
-#         # (1.1, (float, np.float64)),
-#         # (True, (bool, np.bool_)),
-#         # ("a", (str, np.str_)),
-#     ],
-# )
-# def test_np_type_equality(py_val, dtypes):
-#     # numpy type should be the same as the python type
-#     assert np.dtype(dtypes[0]) == dtypes[1]
-#     for dtype in dtypes:
-#         # numpy type should be the same as the inferred python type
-#         assert np.array(py_val).dtype.type == np.dtype(dtype)
 
 
 # Test python type to numpy type conversion and euqality
@@ -120,7 +75,7 @@ def test_enum_conversion():
         ("a", pd.StringDtype),
     ],
 )
-def test_np_type_equality(py_val, dtype):
+def test_pd_type_equality(py_val, dtype):
     # numpy type should be the same as the python type
     assert type(pd.array([py_val]).dtype) == dtype
 
@@ -133,8 +88,8 @@ def get_atomic_strings():
         "Booly": "True",
         "Datey": "2021-01-01",
         "Datetimey": "2021-01-01 00:00:00",
-        "Null": None,
-        # These will always be imported as NaN
+        "Nully": None,
+        # These will always be imported as NA
         # "Empty": "",
         # "Noney": "None",
         # "Nully": "null",
@@ -155,15 +110,11 @@ def get_atomic_string_frames():
     return res
 
 
-def get_atomic_number_arrays():
+def get_atomic_number_frames():
     res = {
-        # f"{num_type.__name__.capitalize()}({num_val})": np.array(num_val)
-        f"{num_type.name.capitalize()}({num_val})": pd.array(
-            [num_val], dtype=num_type.name
+        f"{num_type.name.capitalize()}({num_val})": pd.DataFrame(
+            {num_type.__name__: [num_val]}, dtype=num_type.name
         )
-        # .astype(np.dtype(num_type))
-        # .item()
-        # for num_type in [int, np.int32, np.int64, bool, np.bool_, float, np.float64]
         for num_type in [pd.Float64Dtype, pd.Int64Dtype]
         for num_val in [-1, 0, 1, None]
     }
@@ -171,25 +122,19 @@ def get_atomic_number_arrays():
     return res
 
 
-# def get_atomic_nans():
-#     atomic_NaNs = {
-#         f"NaN({num_type.__name__.capitalize()})": np.array(np.nan)
-#         .astype(np.dtype(num_type))
-#         .item()
-#         # for num_type in [int, np.int32, np.int64, bool, np.bool_, float, np.float64]
-#         for num_type in [float, np.float64]
-#     }
-#     return atomic_NaNs
+def get_atomic_bool_frames():
+    res = {
+        f"{num_type.name.capitalize()}({num_val})": pd.DataFrame(
+            {num_type.__name__: [num_val]}, dtype=num_type.name
+        )
+        for num_type in [pd.BooleanDtype]
+        for num_val in [True, False, None]
+    }
 
-
-def get_atomic_str_nans():
-    atomic_NaNs = {"NaN(Object)": np.array(np.nan).astype("object").item()}
-
-    return atomic_NaNs
+    return res
 
 
 def get_1r1c_tests(atomics: dict):
-
     test_frames = [
         Test(
             f"1r1c{name}",
@@ -197,54 +142,15 @@ def get_1r1c_tests(atomics: dict):
             {"quoting": quoting},
         )
         for name, df in atomics.items()
-        # for quoting in [0, 1, 2, 3]
-        for quoting in [0]
-        # TODO: consider adding 4 and 5 for later versions
-        # if not (
-        #     quoting == 3
-        #     # and (val in ("", None))
-        #     and (
-        #         df[0]
-        #         in (pd.NA)
-        #         # or ((not isinstance(pd_arr, str)) and (np.isnan(pd_arr)))
-        #     )  # or (np.isnan(val)))
-        #     # or ((not isinstance(val, str)) and np.isnan(val))
-        # )
+        for quoting in [0, 1, 2, 3]
+        # single empty field record must be quoted
+        if not (quoting == 3 and df.size == 1 and pd.isna(df.iloc[0, 0]))
     ]
 
     return test_frames
 
 
-# def get_1r1c_tests(atomics: dict):
-
-#     test_frames = [
-#         Test(
-#             f"1r1c{type(val).__name__.capitalize()}{name}",
-#             pd.DataFrame(
-#                 {name: [val]},
-#                 dtype="string" if "Object" in name or "Str" in name else None,
-#             ),
-#             {"quoting": quoting},
-#         )
-#         for name, val in atomics.items()
-#         # for quoting in [0, 1, 2, 3]
-#         for quoting in [0]
-#         # consider adding 4 and 5 for later versions
-#         if not (
-#             quoting == 3
-#             # and (val in ("", None))
-#             and (
-#                 val in ("", None) or ((not isinstance(val, str)) and (np.isnan(val)))
-#             )  # or (np.isnan(val)))
-#             # or ((not isinstance(val, str)) and np.isnan(val))
-#         )
-#     ]
-
-#     return test_frames
-
-
 def id_func(testcase_vals):
-    # if isinstance(testcase_vals, Test):
     return "_".join(
         (
             f"{name if not (name == 'name' or isinstance(value,dict) ) else ''}"
@@ -253,7 +159,6 @@ def id_func(testcase_vals):
         for name, value in testcase_vals._asdict().items()
         if not isinstance(value, pd.DataFrame)
     )
-    # return ""
 
 
 def round_trip_csv(test_case: Test, request):
@@ -278,12 +183,14 @@ def round_trip_csv(test_case: Test, request):
     df2 = pandas_end_points[test_name]
 
     logger.info(df.dtypes)
-    logger.info(df2.dtypes)
+    logger.info(df)
+    # logger.info(df2.dtypes)
+    # logger.info(df2)
 
     assert df.equals(df2)
 
-    # os.remove(test_eel)
-    # os.remove(test_file)
+    os.remove(test_eel)
+    os.remove(test_file)
 
 
 def round_trip_excel(test_name: str, df: pd.DataFrame):
@@ -331,9 +238,8 @@ class TestExcel:
 
 test_classes = {
     "TestString": get_atomic_string_frames,
-    # "TestNumber": get_atomic_number_arrays,
-    # "TestStrNan": get_atomic_str_nans,
-    # "TestNan": get_atomic_nans,
+    "TestNumber": get_atomic_number_frames,
+    "TestBool": get_atomic_bool_frames,
 }
 
 for class_name, func in test_classes.items():
