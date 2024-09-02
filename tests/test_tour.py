ed31b4c62ca66706d473efb7787e0c37227fdf7e
Nathan Walsh <nwalsh@stratence.partners>
2024-09-02 11:34:25 +0200

    doc tests working

--- tests/test_tour.py
+++ tests/test_tour.py
@@ -2,17 +2,12 @@ import subprocess
 
 
 def test_eel_tree(tmp_path, capsys):
-    # import subprocess
+    subprocess.run(["pwsh", "D:\\Sync\\repos\\eel\\tests\\docs\\generate_example.ps1"])
 
-    # print("a")
     result = subprocess.run(
         ["git", "diff", "--name-only", "."],
         capture_output=True,
         text=True,
         cwd="D:\\Sync\\repos\\eel\\tests\\docs\\controls",
     )
-    # captured = capsys.readouterr()
-    # expected_output = "a\n"
-    # assert captured.out == expected_output
-    assert result != ""
-    # assert tmp_path.exists()
+    assert result.stdout == ""
